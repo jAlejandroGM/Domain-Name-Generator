2f9730b9d1aa2c541175bc6d71a45cd8ba881bc0
Alejandro Guzmán <j.alejandro.guzman.m@gmail.com>
2025-04-10 22:01:32 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the', 'our','incredibly','unbelivable',];
+let adj = ['great', 'big','enormous','tiny','weird'];
+let noun = ['jogger', 'racoon','cat','whale','alien'];
+let extensions = ['.com', '.net', '.us', '.io','.pe'];
+
+let domainArray = []
+
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let e of extensions) {
+        domainArray.push(p + a + n + e);
+      }
+    }
+  }
+}
+
+let domain = domainArray.join("<br>")
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector('#domain').innerHTML = domain;
+
 };
